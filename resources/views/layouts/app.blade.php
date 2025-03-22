c1233fe2db03c74a8c2b78d0901ef0e74f0af771
alvindioprakosa <alvindioprakosa@gmail.com>
2025-03-22 22:48:46 +0700

    Update App

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,5 +1,6 @@
 <!doctype html>
 <html lang="{{ app()->getLocale() }}">
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -19,30 +20,35 @@
 
     <!-- Styles -->
     <link href="{{ mix('css/app.css') }}" rel="stylesheet">
-    <link rel="stylesheet" href="{{asset('custom/css/app.css')}}" />
+    <link rel="stylesheet" href="{{ asset('custom/css/app.css') }}" />
 </head>
+
 <body class="py-5 md:py-0">
 
-        @include('layouts._particles._sidebar-mobile')
-   
-        @include('layouts._particles._topbar')
-        <div class="flex overflow-hidden">
+    @include('layouts._particles._sidebar-mobile')
 
-            @include('layouts._particles._sidebar')
-           
-            <div class="content" id="app" v-cloak>
-                 @yield('content')
-             
-                @if(App\Helpers\GitVersionHelper::version())
-                    <footer>
-                        <div class=" py-1 text-sm text-grey-500 text-center"><a href="https://github.com/anonaddy/anonaddy/releases/tag/v{{ App\Helpers\GitVersionHelper::version() }}" target="_blank" rel="nofollow noreferrer noopener" class="block sm:inline">v{{ App\Helpers\GitVersionHelper::version() }}</a></div>
-                    </footer>
-                @endif
-                <notifications position="bottom right" /> 
-            </div>
+    @include('layouts._particles._topbar')
+    <div class="flex overflow-hidden">
+
+        @include('layouts._particles._sidebar')
+
+        <div class="content" id="app" v-cloak>
+            @yield('content')
+
+            @if (App\Helpers\GitVersionHelper::version())
+                <footer>
+                    <div class=" py-1 text-sm text-grey-500 text-center"><a
+                            href="https://github.com/anonaddy/anonaddy/releases/tag/v{{ App\Helpers\GitVersionHelper::version() }}"
+                            target="_blank" rel="nofollow noreferrer noopener"
+                            class="block sm:inline">v{{ App\Helpers\GitVersionHelper::version() }}</a></div>
+                </footer>
+            @endif
+            <notifications position="bottom right" />
         </div>
-       
-    <script src="{{asset('custom/js/app.js')}}"></script>
+    </div>
+
+    <script src="{{ asset('custom/js/app.js') }}"></script>
     <script src="{{ mix('js/app.js') }}"></script>
 </body>
+
 </html>
