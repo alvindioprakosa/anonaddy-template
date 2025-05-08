2e0180f1deba3e4ecdc2d3fa238db17cfff77aaa
alvindioprakosa <alvindioprakosa@gmail.com>
2025-05-08 23:44:46 +0700

    Update Status

--- resources/views/shared/status.blade.php
+++ resources/views/shared/status.blade.php
@@ -1,5 +1,8 @@
 @if (session('status'))
-    <div class="text-sm border-t-8 rounded text-green-600 border-green-500 bg-green-100 px-3 py-4 mb-4" role="alert">
+    <div 
+        class="mb-4 rounded border-t-8 border-green-500 bg-green-100 px-3 py-4 text-sm text-green-600" 
+        role="alert"
+    >
         {{ session('status') }}
     </div>
 @endif
